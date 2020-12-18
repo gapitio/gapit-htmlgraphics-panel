286c44f8f380a23c71951a17e8282a830fd3aa8f
ZuperZee <ole.kristian.kaaseth@gmail.com>
2020-12-18 14:38:52 +0100

    feat: add download panel options button

--- src/PanelOptionImportExport.tsx
+++ src/PanelOptionImportExport.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { StandardEditorProps } from '@grafana/data';
 import { OptionsInterface, EditorLanguageType } from './types';
 import { TextPanelEditor } from './TextPanelEditor';
+import { Button } from '@grafana/ui';
+import { exportFile, contentType } from 'components/exportFile';
+import { EditorCodeType } from 'types';
 
 interface Settings {
   language: EditorLanguageType;
 }
 
 interface Props extends StandardEditorProps<OptionsInterface['importedPanelOptions'] | undefined, Settings> {}
+
+function downloadJsonFile(panelOptionString: EditorCodeType) {
+  if (panelOptionString) {
+    exportFile(panelOptionString, 'panel-options.json', contentType.json);
+  } else {
+    console.warn('Panel options is empty. Failed download.');
+  }
+}
 
 export const PanelOptionImportedPanelOptions: React.FC<Props> = ({ value, item, onChange, context }) => {
   if (context.options) {
@@ -19,6 +30,9 @@ export const PanelOptionImportedPanelOptions: React.FC<Props> = ({ value, item, 
 
   return (
     <div>
+      <Button style={{ marginBottom: '6px' }} onClick={() => downloadJsonFile(value)}>
+        Download as JSON file
+      </Button>
       <TextPanelEditor
         language={item.settings?.language}
         value={value}
---
+++ src/components/exportFile.tsx
@@ -0,0 +1,20 @@
+enum contentType {
+  json = 'application/json;charset=utf-8;',
+}
+
+function exportFile(string: string, filename: string, contentType: contentType) {
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    var blob = new Blob([decodeURIComponent(encodeURI(string))], { type: contentType });
+    navigator.msSaveOrOpenBlob(blob, filename);
+  } else {
+    var a = document.createElement('a');
+    a.download = filename;
+    a.href = 'data:' + contentType + ',' + encodeURIComponent(string);
+    a.target = '_blank';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+}
+
+export { exportFile, contentType };
