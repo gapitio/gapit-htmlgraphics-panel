75a6229ae09ea69d328fd30cdd92aaf666fb4af5
Ole Kristian (Zee) <ole.kristian.kaaseth@gmail.com>
2021-06-29 12:28:34 +0200

    refac(exportFile): generalize download method (#27)

--- src/utils/exportFile.ts
+++ src/utils/exportFile.ts
@@ -3,16 +3,11 @@ export enum contentType {
 }
 
 export function exportFile(string: string, filename: string, contentType: contentType) {
-  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-    var blob = new Blob([decodeURIComponent(encodeURI(string))], { type: contentType });
-    navigator.msSaveOrOpenBlob(blob, filename);
-  } else {
-    var a = document.createElement('a');
-    a.download = filename;
-    a.href = 'data:' + contentType + ',' + encodeURIComponent(string);
-    a.target = '_blank';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
+  const a = document.createElement('a');
+  a.download = filename;
+  a.href = 'data:' + contentType + ',' + encodeURIComponent(string);
+  a.target = '_blank';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
