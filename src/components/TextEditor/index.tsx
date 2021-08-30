5b06fdee1d76ab33575dd9b90d0da7b76088f4c7
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-08-30 15:02:49 +0200

    feat: use integrated code editor

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,17 +1,7 @@
 import React, { Component } from 'react';
-import MonacoEditor, { BeforeMount, loader, OnMount } from '@monaco-editor/react';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import { EditorLanguageType } from 'types';
-import { config } from '@grafana/runtime';
-import textEditorDeclarations from './declarations';
-
-// Load the monaco files locally
-// https://github.com/suren-atoyan/monaco-react#loader-config
-loader.config({
-  paths: {
-    vs: 'public/plugins/gapit-htmlgraphics-panel/lib/vs',
-  },
-});
+import { CodeEditor } from '@grafana/ui';
 
 interface Props {
   language: EditorLanguageType;
@@ -34,24 +24,6 @@ export class TextEditor extends Component<Props, State> {
     this.state.editor?.layout();
   }
 
-  editorWillMount: BeforeMount = (monaco) => {
-    if (this.props.language === 'javascript') {
-      // Add autocompletion for panel definitions (htmlNode, codeData, data, options, and theme)
-      monaco.languages.typescript.javascriptDefaults.addExtraLib(textEditorDeclarations);
-    }
-  };
-
-  editorOnMount: OnMount = (editor, monaco) => {
-    // Save editor value when CTRL+S are pressed inside the editor
-    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
-      this.saveEditorValue();
-    });
-
-    this.setState({
-      editor: editor,
-    });
-  };
-
   saveEditorValue = () => {
     if (this.state.editor) {
       this.props.onChange(this.state.editor.getValue());
@@ -61,15 +33,7 @@ export class TextEditor extends Component<Props, State> {
   render = () => {
     return (
       <div onBlur={this.saveEditorValue}>
-        <MonacoEditor
-          height={'33vh'}
-          language={this.props.language}
-          theme={config.theme.isDark ? 'vs-dark' : 'vs-light'}
-          value={this.props.value}
-          beforeMount={this.editorWillMount}
-          onMount={this.editorOnMount}
-          options={{ fontSize: 12 }}
-        />
+        <CodeEditor height={'33vh'} value={this.props.value ?? ''} language={this.props.language ?? ''} />
       </div>
     );
   };
