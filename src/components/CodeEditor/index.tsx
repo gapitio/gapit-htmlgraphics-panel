46b2030fba303b6f9942bfbe4e91b94604372285
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-10-08 15:10:55 +0200

    refac(editor): simplify code

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
 import { EditorLanguageType } from 'types';
 import { CodeEditor as GrafanaCodeEditor, Monaco } from '@grafana/ui';
@@ -10,42 +10,24 @@ interface Props {
   onChange: (value?: string) => void;
 }
 
-interface State {
-  editor?: editor.IStandaloneCodeEditor;
-}
-
-export class CodeEditor extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  editorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
-    if (this.props.language === 'javascript') {
+export const CodeEditor: FC<Props> = ({ language, value, onChange }) => {
+  const editorDidMount = (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+    if (language === 'javascript') {
       // Add autocompletion for panel definitions (htmlNode, codeData, data, options, ETC)
       monaco.languages.typescript.javascriptDefaults.addExtraLib(declarations, 'HtmlGraphics/HtmlGraphics.d.ts');
     }
-
-    this.setState({ editor });
   };
 
-  saveEditorValue = (value: string) => {
-    this.props.onChange(value);
-  };
-
-  render = () => {
-    return (
-      <GrafanaCodeEditor
-        height={'33vh'}
-        value={this.props.value ?? ''}
-        language={this.props.language ?? ''}
-        showLineNumbers={true}
-        onEditorDidMount={this.editorDidMount}
-        onSave={this.saveEditorValue}
-        onBlur={this.saveEditorValue}
-        monacoOptions={{ contextmenu: true }}
-      />
-    );
-  };
-}
+  return (
+    <GrafanaCodeEditor
+      height={'33vh'}
+      value={value ?? ''}
+      language={language ?? ''}
+      showLineNumbers={true}
+      onEditorDidMount={editorDidMount}
+      onSave={onChange}
+      onBlur={onChange}
+      monacoOptions={{ contextmenu: true }}
+    />
+  );
+};
