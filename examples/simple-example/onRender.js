4eb044ca94deb2c91ff1e4565ef2aacb9becb022
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-10-11 06:51:28 +0200

    chore: remove examples The website contains examples.

--- examples/simple-example/onRender.js
+++
@@ -1,6 +0,0 @@
-// Sets the value from the first series on every refresh
-const htmlgraphicsValue = htmlNode.getElementById('htmlgraphics-value');
-
-if (htmlgraphicsValue) {
-  htmlgraphicsValue.textContent = data.series[0].fields[1].state.calcs.last;
-}
