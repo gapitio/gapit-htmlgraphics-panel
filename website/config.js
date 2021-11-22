c57de1e7dff27297445c5ba62b23185d37419913
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-11-22 10:47:35 +0100

    docs: use variables for version number and url

---
+++ website/config.js
@@ -0,0 +1,6 @@
+module.exports.htmlGraphics = {
+  version: '1.5.0',
+  releaseUrl: function () {
+    return `https://github.com/gapitio/gapit-htmlgraphics-panel/releases/download/v${this.version}/gapit-htmlgraphics-panel-${this.version}.zip`;
+  },
+};
