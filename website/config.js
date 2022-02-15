fcb71665c4486de0d0234bf7911f26ecc0a2ae48
ZuperZee <ole.kristian.kaaseth@gmail.com>
2022-02-15 13:49:19 +0100

    chore(website): update htmlgraphics version number

--- website/config.js
+++ website/config.js
@@ -1,5 +1,5 @@
 module.exports.htmlGraphics = {
-  version: '2.0.0',
+  version: '2.0.2',
   releaseUrl: function () {
     return `https://github.com/gapitio/gapit-htmlgraphics-panel/releases/download/v${this.version}/gapit-htmlgraphics-panel-${this.version}.zip`;
   },
