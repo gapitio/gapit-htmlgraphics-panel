0b03d76cac18a9da9f02ce28d6e2142a05012c52
Ole Kristian (Zee) <ole.kristian.kaaseth@gmail.com>
2022-08-05 09:48:23 +0200

    refac(decl): use .d.ts as declaration files (#113)

---
+++ src/components/CodeEditor/declarations/@grafana/runtime.d.ts
@@ -0,0 +1,96 @@
+import { VariableModel, ScopedVars, TimeRange, UrlQueryMap } from './data';
+
+/**
+ * @public
+ * @deprecated in favor of {@link locationService} and will be removed in Grafana 9
+ */
+export interface LocationUpdate {
+  /**
+   * Target path where you automatically wants to navigate the user.
+   */
+  path?: string;
+  /**
+   * Specify this value if you want to add values to the query string of the URL.
+   */
+  query?: UrlQueryMap;
+  /**
+   * If set to true, the query argument will be added to the existing URL.
+   */
+  partial?: boolean;
+  /**
+   * Used internally to sync the Redux state from Angular to make sure that the Redux location
+   * state is in sync when navigating using the Angular router.
+   *
+   * @remarks
+   * Do not change this unless you are the Angular router.
+   *
+   * @internal
+   */
+  routeParams?: UrlQueryMap;
+  replace?: boolean;
+}
+/**
+ * If you need to automatically navigate the user to a new place in the application this should
+ * be done via the LocationSrv and it will make sure to update the application state accordingly.
+ *
+ * @public
+ * @deprecated in favor of {@link locationService} and will be removed in Grafana 9
+ */
+export interface LocationSrv {
+  update(options: LocationUpdate): void;
+}
+/**
+ * Used during startup by Grafana to set the LocationSrv so it is available
+ * via the {@link getLocationSrv} to the rest of the application.
+ *
+ * @internal
+ */
+export declare function setLocationSrv(instance: LocationSrv): void;
+/**
+ * Used to retrieve the {@link LocationSrv} that can be used to automatically navigate
+ * the user to a new place in Grafana.
+ *
+ * @public
+ * @deprecated in favor of {@link locationService} and will be removed in Grafana 9
+ */
+export declare function getLocationSrv(): LocationSrv;
+
+/**
+ * Via the TemplateSrv consumers get access to all the available template variables
+ * that can be used within the current active dashboard.
+ *
+ * For a more in-depth description visit: https://grafana.com/docs/grafana/latest/reference/templating
+ * @public
+ */
+export interface TemplateSrv {
+  /**
+   * List the dashboard variables
+   */
+  getVariables(): VariableModel[];
+  /**
+   * Replace the values within the target string.  See also {@link InterpolateFunction}
+   */
+  replace(target?: string, scopedVars?: ScopedVars, format?: string | Function): string;
+  /**
+   * Checks if a target contains template variables.
+   */
+  containsTemplate(target?: string): boolean;
+  /**
+   * Update the current time range to be used when interpolating __from / __to variables.
+   */
+  updateTimeRange(timeRange: TimeRange): void;
+}
+/**
+ * Used during startup by Grafana to set the TemplateSrv so it is available
+ * via the {@link getTemplateSrv} to the rest of the application.
+ *
+ * @internal
+ */
+export declare const setTemplateSrv: (instance: TemplateSrv) => void;
+/**
+ * Used to retrieve the {@link TemplateSrv} that can be used to fetch available
+ * template variables.
+ *
+ * @public
+ */
+export declare const getTemplateSrv: () => TemplateSrv;
---
+++ src/components/CodeEditor/declarations/global.d.ts
@@ -0,0 +1,50 @@
+import {
+  FieldDisplay,
+  FieldReducerInfo,
+  GrafanaTheme,
+  GrafanaTheme2,
+  PanelData,
+  PanelProps,
+  Registry,
+} from '@grafana/data';
+import { getLocationSrv as getLocationSrvType, getTemplateSrv as getTemplateSrvType } from '@grafana/runtime';
+import { HTMLNode, JSONType, OptionsInterface, PopulatedGetFieldDisplayValuesOptions } from './index';
+
+declare global {
+  /**
+   * The ShadowRoot containing the HTML/SVG and CSS code.
+   *
+   * The htmlNode is a shadow root https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM.
+   */
+  const htmlNode: HTMLNode;
+  const codeData: JSONType;
+  const customProperties: JSONType;
+  const data: PanelData;
+  const options: OptionsInterface;
+  const theme: GrafanaTheme;
+  const getTemplateSrv: typeof getTemplateSrvType;
+  const getLocationSrv: typeof getLocationSrvType;
+
+  const htmlGraphics: {
+    /**
+     * The ShadowRoot containing the HTML/SVG and CSS code.
+     *
+     * The htmlNode is a shadow root https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM.
+     */
+    htmlNode: typeof htmlNode;
+    codeData: typeof codeData;
+    customProperties: typeof customProperties;
+    data: typeof data;
+    options: typeof options;
+    theme: typeof theme;
+    theme2: GrafanaTheme2;
+    getTemplateSrv: typeof getTemplateSrv;
+    getLocationSrv: typeof getLocationSrv;
+    props: PanelProps<OptionsInterface>;
+    width: number;
+    height: number;
+    getFieldDisplayValues: (options: PopulatedGetFieldDisplayValuesOptions) => FieldDisplay[];
+    fieldDisplayValues: FieldDisplay[];
+    fieldReducers: Registry<FieldReducerInfo>;
+  };
+}
