80efed867c9f7dbfb18e2d0be56394b564db8e89
ZuperZee <ole.kristian.kaaseth@gmail.com>
2024-04-19 17:41:19 +0200

    refac: migrate to grafana/create-plugin
    https://github.com/grafana/plugin-tools
    https://grafana.com/developers/plugin-tools/migration-guides/migrate-from-toolkit
    Ran pnpm dlx @grafana/create-plugin@latest migrate

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,4 @@
-// jshint esversion: 9
-
 module.exports = {
-  ...require('./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json'),
+  // Prettier configuration provided by Grafana scaffolding
+  ...require('./.config/.prettierrc.js'),
 };
