f0fb9f858abab5691d2e7beaaa393aeae67d3098
ZuperZee <ole.kristian.kaaseth@gmail.com>
2025-01-09 09:51:52 +0100

    fix: don't transpile declarations
    Happened when updating the grafana plugin. The webpack rules changed.
    Now the correct rule is modified meaning declarations are not
    transpiled anymore.

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,13 +13,13 @@ const config = async (env): Promise<Configuration> => {
     if (!Array.isArray(baseConfig.module.rules)) {
       throw new Error('Expected baseConfig.module.rules to be an array');
     }
-    if (baseConfig.module.rules.length === 0) {
+    if (baseConfig.module.rules.length < 2) {
       throw new Error('baseConfig.module.rules is missing rules');
     }
-    const javascriptParserRule = baseConfig.module.rules[0];
+    const javascriptParserRule = baseConfig.module.rules[1];
 
     if (javascriptParserRule == null || typeof javascriptParserRule !== 'object') {
-      throw new Error('Expected baseConfig.module.rules[0] to be an object (RuleSetRule)');
+      throw new Error('Expected baseConfig.module.rules[1] to be an object (RuleSetRule)');
     }
     javascriptParserRule.exclude = [/node_modules/, htmlGraphicsDeclarationsPath];
   } else {
