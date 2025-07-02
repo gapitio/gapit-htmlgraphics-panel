9c48030de631e67af8b41994b384d94e5d2f4d11
ZuperZee <ole.kristian.kaaseth@gmail.com>
2025-07-02 12:38:22 +0200

    docs: make latest version v2.2.1

--- website/config.js
+++ website/config.js
@@ -1,5 +1,5 @@
 module.exports.htmlGraphics = {
-  version: '2.2.0',
+  version: '2.2.1',
   latestReleaseUrl:
     'https://github.com/gapitio/gapit-htmlgraphics-panel/releases/latest/download/gapit-htmlgraphics-panel.zip',
   releaseUrl: function () {
