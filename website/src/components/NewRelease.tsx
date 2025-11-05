2aa7d6647cf356574db2f08b7943d1430600f32a
ZuperZee <ole.kristian.kaaseth@gmail.com>
2025-11-05 14:16:45 +0100

    docs(website): make new release notes any patch

--- website/src/components/NewRelease.tsx
+++ website/src/components/NewRelease.tsx
@@ -5,7 +5,7 @@ export function NewRelease() {
   return (
     <div className="container">
       <h1 style={{ marginTop: '1rem', textAlign: 'center' }}>
-        <Link to="/docs/release-notes/v2.2.0">HTMLGraphics v2.2.0 is out!</Link>
+        <Link to="/docs/release-notes/v2.2.0">HTMLGraphics v2.2.x is out!</Link>
       </h1>
     </div>
   );
